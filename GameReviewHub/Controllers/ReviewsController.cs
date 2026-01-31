8a4e389f44bdfc2fcfcd057f8688c4d330a0a45a
KamenBodzhakov <everdr3am@gmail.com>
2026-01-31 23:07:26 +0200

    Added ReviewsController with ByGame action

---
+++ GameReviewHub/Controllers/ReviewsController.cs
@@ -0,0 +1,41 @@
+﻿using GameReviewHub.Data;
+using GameReviewHub.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameReviewHub.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly GameReviewHubDbContext dbContext;
+
+        public ReviewsController(GameReviewHubDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ByGame(int gameId)
+        {
+            Game? game = dbContext
+                .Games
+                .Include(g => g.Reviews)
+                .AsSplitQuery()
+                .AsNoTracking()
+                .FirstOrDefault(g => g.Id == gameId);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+    }
+}
